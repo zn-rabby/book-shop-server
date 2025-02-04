e8da5d4670e7ee597245172f0aebddd98ed632d2
rabby-web <rabby.webdeveloper@gmail.com>
2025-02-04 19:20:18 +0600

    link add

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['https://book-shop-client-seven.vercel.app'],
     credentials: true,
   }),
 );
